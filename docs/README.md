6dd9dfdb8a4eb9b0400d9c9665d82081a4fc3916
learningcoding-pixel <yinweihaowork@gmail.com>
2025-08-26 20:32:30 +0800

    Add src/main/java/.idea/

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Meow User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Meow.java
@@ -0,0 +1,10 @@
+public class Meow {
+    public static void main(String[] args) {
+        String greet = "Hello! I am Meow. ᓚᘏᗢ\n"
+                     + "What can I do for you?\n";
+
+        String bye = "Bye. Hope to see you again soon!";
+        System.out.println(greet);
+        System.out.println(bye);
+    }
+}
