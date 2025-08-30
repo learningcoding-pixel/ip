fb3598344d75f4627b29b1a5494c88811d5a705f
learningcoding-pixel <yinweihaowork@gmail.com>
2025-08-30 22:13:01 +0800

    commit files for Level 1.Echo

--- src/main/java/Meow.java
+++ src/main/java/Meow.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;  // Import the Scanner class
+
 public class Meow {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
         String greet = "Hello! I am Meow. ᓚᘏᗢ\n"
                      + "What can I do for you?\n";
+        String ask = "Enter your input for me to Miaow back or enter bye to leave: ";
+        String bye = "Bye. Hope to see you again soon with Chicken Cat Treats ᓚᘏᗢ\n";
 
-        String bye = "Bye. Hope to see you again soon!";
         System.out.println(greet);
+        System.out.println(ask);
+        String userInput = scanner.nextLine();  // Read user input
+
+        while(!userInput.equals("bye")){
+            System.out.println(userInput);
+            System.out.println(ask);
+            userInput = scanner.nextLine();
+        }
+
+        scanner.close();//close scanner
         System.out.println(bye);
     }
 }
