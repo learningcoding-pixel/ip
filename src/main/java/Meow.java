07fecefc58b5d8f275cf7215b8c98552fe27cf9d
learningcoding-pixel <yinweihaowork@gmail.com>
2025-08-30 22:51:52 +0800

    commit files for Level-2

--- src/main/java/Meow.java
+++ src/main/java/Meow.java
@@ -4,9 +4,13 @@ public class Meow {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        String[] userInputs = new String[100]; //Create fixed sized array of 100 to store items
+        int counter = 0;
         String greet = "Hello! I am Meow. ᓚᘏᗢ\n"
                      + "What can I do for you?\n";
-        String ask = "Enter your input for me to Miaow back or enter bye to leave: ";
+        String ask = "Enter any input for me to record down\n"
+                   + "or <list> to show all your recorded inputs\n"
+                   + "or <bye> to exit.";
         String bye = "Bye. Hope to see you again soon with Chicken Cat Treats ᓚᘏᗢ\n";
 
         System.out.println(greet);
@@ -14,7 +18,18 @@ public class Meow {
         String userInput = scanner.nextLine();  // Read user input
 
         while(!userInput.equals("bye")){
-            System.out.println(userInput);
+
+            if (userInput.equals("list")){
+                for(int i = 0; i < counter; i++){
+                    System.out.println((i + 1) + ": " + userInputs[i]);
+                }
+            }else{
+                System.out.println("Recorded: " + userInput);
+                userInputs[counter] = userInput;
+                counter += 1;
+            }
+
+            System.out.println();
             System.out.println(ask);
             userInput = scanner.nextLine();
         }
