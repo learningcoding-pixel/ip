e634f880d81f89f7fd24ef74ba995dd1538810ba
learningcoding-pixel <yinweihaowork@gmail.com>
2025-08-31 23:48:10 +0800

    Commit for Level 6

--- src/main/java/Meow.java
+++ src/main/java/Meow.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;  // Import the Scanner class
+import java.util.ArrayList;
 
 public class Meow {
 
-    public static void printAll(Task[] tasks, int counter){
-        for (int i = 0; i < counter; i++) {
-            System.out.println((i + 1) + ". " + tasks[i].getStatus());
+    public static void printAll(ArrayList<Task> tasks) {
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i).getStatus());
         }
     }
 
@@ -12,8 +13,7 @@ public class Meow {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);  // Create a Scanner object
-        Task[] tasks = new Task[100]; //Create fixed sized array of 100 to store items
-        int counter = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
         char last;
         int num;
         String name;
@@ -42,27 +42,28 @@ public class Meow {
 
 
                 if (userInput.equals("list")) {
-                    printAll(tasks,counter);
+                    printAll(tasks);
                 }else if(userInput.contains("unmark")) {
-                    tasks[num - 1].setUnDone();
-                    printAll(tasks,counter);
-                }else if(userInput.contains("mark")){
-                    tasks[num - 1].setDone();
-                    printAll(tasks,counter);
+                    tasks.get(num - 1).setUnDone();
+                    printAll(tasks);
+                }else if(userInput.contains("mark")) {
+                    tasks.get(num - 1).setDone();
+                    printAll(tasks);
+                }else if(userInput.contains("delete")){
+                    tasks.remove(num - 1);
+                    printAll(tasks);
                 }else if(userInput.contains("todo")) {
                     name = userInput.substring(5);
                     Todo todo = new Todo(name);
-                    tasks[counter] = todo;
-                    counter += 1;
-                    printAll(tasks, counter);
+                    tasks.add(todo);
+                    printAll(tasks);
                 }else if(userInput.contains("deadline")) {
                     int index = userInput.indexOf('/');
                     name = userInput.substring(9, index);
                     String endDate = userInput.substring(index + 1);
                     Deadline deadline = new Deadline(name, endDate);
-                    tasks[counter] = deadline;
-                    counter += 1;
-                    printAll(tasks, counter);
+                    tasks.add(deadline);
+                    printAll(tasks);
                 } else if(userInput.contains("event")){
                     int index = userInput.indexOf('/');
                     int lastIndex = userInput.lastIndexOf('/');
@@ -70,14 +71,12 @@ public class Meow {
                     String startDate = userInput.substring(index + 1,lastIndex);
                     String endDate = userInput.substring(lastIndex + 1);
                     Event event = new Event(name,startDate,endDate);
-                    tasks[counter] = event;
-                    counter += 1;
-                    printAll(tasks,counter);
+                    tasks.add(event);
+                    printAll(tasks);
                 }else{
                     System.out.println("Recorded: " + userInput);
                     Task task = new Task(userInput);
-                    tasks[counter] = task;
-                    counter += 1;
+                    tasks.add(task);
                 }
 
 
